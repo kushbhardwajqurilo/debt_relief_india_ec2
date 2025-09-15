dfb8a317c978fcbb91927a503552d5e271a1675a
kush <=>
2025-09-15 10:33:21 +0530

    opt console remove

--- src/controllers/userControll.js
+++ src/controllers/userControll.js
@@ -36,8 +36,6 @@ exports.sendOTP = async (req, res) => {
     const apiUrl = `https://sms.autobysms.com/app/smsapi/index.php?key=45FA150E7D83D8&campaign=0&routeid=9&type=text&contacts=${phone}&senderid=SMSSPT&msg=Your OTP is ${otp} SELECTIAL&template_id=1707166619134631839`;
 
     const response = await axios.get(apiUrl);
-    console.log("SMS API Response:", response.data);
-
     if (response.data.type === "SUCCESS") {
       return res.status(200).json({
         success: true,
