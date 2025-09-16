b1a50465caf83dc51e56149a1905fe25a1deb48a
kush <=>
2025-09-16 12:06:11 +0530

    udpate

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -77,5 +77,5 @@ adminRouter.post("/request-otp", AuthMiddleWare, requestOtp);
 adminRouter.post("/verify-otp", AuthMiddleWare, verifyOtpForAdmin);
 adminRouter.put("/change-password", forgetPasswordMddleware, changePasswprd);
 adminRouter.post("/call-now-feature", callNowSetup);
-adminRouter.get('/get-advocate-call',getYourContactCall;)
+adminRouter.get("/get-advocate-call", getYourContactCall);
 module.exports = adminRouter;
